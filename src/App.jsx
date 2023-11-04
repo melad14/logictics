8f487808da6b90e85c15f7cd530384212af041e9
melad14 <miladshehata513@gmail.com>
2023-11-04 20:39:33 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ export default function App() {
   <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<Home/>}/>
+      <Route path='logictics' element={<Home/>}/>
       <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/>
       <Route path='services' element={<Services/>}/>
